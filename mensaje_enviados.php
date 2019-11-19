209a808423117670ae0ce18ce25f8683c18511aa
Gabrielm-git <57098544+Gabrielm-git@users.noreply.github.com>
2019-11-18 23:16:36 -0300

    Create mensaje_enviados.php

---
+++ mensaje_enviados.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <title>Trivia Preguntas y Respuestas</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<link href="https://fonts.googleapis.com/css?family=ZCOOL+KuaiLe&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Press+Start+2P&display=swap" rel="stylesheet">
+ 
+
+ <link rel="stylesheet" href="/css/css_contacto.css">
+  <link href="https://fonts.googleapis.com/css?family=ZCOOL+KuaiLe&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Press+Start+2P&display=swap" rel="stylesheet">
+</head>
+<body>
+<?php
+include("header.php");
+?>
+
+        	<div class="container">
+    <section>
+     <h2>  El mensaje fue enviado, en breve nos comunicaremos <h2> 
+    </section>
+  </div>
+
+  
+
+<?php
+include("footer.php");
+?>
+</body>
+</html>
