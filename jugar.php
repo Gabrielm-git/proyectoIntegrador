7b769b0df2371e8a2308b5f220e1fe4fa1d4fa8d
Valentin Ricci <54277400+riccivalentin@users.noreply.github.com>
2019-12-10 02:22:29 -0300

    Add files via upload

--- jugar.php
+++ jugar.php
@@ -9,15 +9,18 @@
     <link rel="stylesheet" href="css/stylepyr.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
   </head>
-  <body>
+  <body style="font-family:Consolas;">
     <?php include_once("header.php"); ?>
     <div class="caja jumbotron bg-light block-center" id="cajita">
     <img class="pyr mx-auto d-block rounded-circle img-thumbnail" id="minipregres"src="img/trivia.jpg" alt="preguntasyrespuestas"><br>
     <h3 class="question text-center">¿Quien convirtió el séptimo gol en la histórica goleada <br> de Estudiantes 7-0 Gimnasia?</h3>
     <hr class="my-4">
-    <div class="progress">
-      <div class="progress-bar bg-warning w-50"><s>50%!</s></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;50% restante!
-    </div>
+    <div class="progress" style="height:40px;">
+      <div class="progress-bar-striped progress-bar-animated bg-warning w-50">
+        <s><p style="text-align:center;padding:8px;font-size: 15px;color:black;">50%!</p></s>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <p style="text-align:center;padding:10px;">50% restante</p></div>
+        <div class="progress-bar-striped bg-dark progress-bar-animated w-50">
+          <p style="text-align:center;padding:8px;font-size: 15px;color:lightyellow;">50% restante</p></div></div>
     <hr class="my-4">
     <div class="botonesr btn-group-vertical mx-auto d-block" id="botones">
       <button type="button" class="btn mx-auto d-block btn-outline-warning btn-lg">Pablo 'El Payaso' Luguercio</button><br>
