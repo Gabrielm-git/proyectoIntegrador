1ab508d26f43357fc6c3d7988a73b871e6b6934f
Gabrielm-git <57098544+Gabrielm-git@users.noreply.github.com>
2019-12-10 01:09:41 -0300

    Update ranking.php

--- ranking.php
+++ ranking.php
@@ -8,7 +8,7 @@
   <link href="https://fonts.googleapis.com/css?family=Press+Start+2P&display=swap" rel="stylesheet">
 
 <!-- Compiled and minified Bootstrap CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" >
 
 </head>
 
