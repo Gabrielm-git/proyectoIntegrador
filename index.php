ae4b60a911e45e781d2a116cfb63036b3f93089b
leoocabaf <leoo.caballero@gmail.com>
2019-12-11 13:58:45 -0300

    Cambio de "title".

--- index.php
+++ index.php
@@ -5,14 +5,14 @@
     <title>Inicio</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Jugar a Trivia - Preguntas y Respuestas!</title>
+    <title>Trivia</title>
     <link rel="stylesheet" href="css/estilo-header.css">
     <link rel="stylesheet" href="css/estilo-footer.css">
   <link rel="stylesheet" href="css/estilo-botonera.css">
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
   </head>
   <body>
-  
+
   <?php include("header.php") ?>
   <?php include("botonera.php") ?>
   <?php include("footer.php") ?>
