e5d535ddd56adf58a53a7a64b9556e5dedcd7089
Juanmedel7 <57683097+Juanmedel7@users.noreply.github.com>
2019-12-11 00:08:26 -0300

    Footer - Login
    Con sus respectivos css

---
+++ footer.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="/css/estilo-footer.css">
+</head>
+<body>
+  <!-- Site footer -->
+
+  <footer class="section bg-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3">
+          <div class="">
+            <h6 class="footer-heading text-uppercase text-white">Mas Información</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
+        </div>
+
+        <div class="col-lg-3">
+          <div class="">
+            <h6 class="footer-heading text-uppercase text-white">Acerca de nosotros</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
+        </div>
+
+        <div class="col-lg-2">
+          <div class="">
+            <h6 class="footer-heading text-uppercase text-white">cuenta</h6>
+            <ul class="list-unstyled footer-link mt-4">
+              <li onclick="location.href = 'login.php'" style="cursor: pointer";>Ingresar</li>
+              <li onclick="location.href = 'login.php'" style="cursor: pointer";>Registrarme</li>
+              <li>      </li>
+              <li></li>
+            </ul>
+
+          </div>
+        </div>
+
+        <div class="col-lg-4">
+          <div class="">
+            <h6 class="footer-heading text-uppercase text-white">redes sociales</h6>
+            <p class="contact-info mt-4">Para más información no dude en contactarnos.</p>
+            <p class="contact-info">+54 221 458-5034</p>
+            <div class="mt-5">
+              <ul class="list-inline">
+                <li class="list-inline-item"><a href="#"><img src="img/facebook.png" alt=""></a></li>
+                <li class="list-inline-item"><a href="#"><img src="img/twitter.png" alt=""></a></li>
+                <li class="list-inline-item"><a href="#"><img src="img/Instagram.png" alt=""></a></li>
+                <li class="list-inline-item"><a href="#"><img src="img/linkedin.png" alt=""></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="text-center mt-5">
+      <p class="footer-alt mb-0 f-14">2019 Copyright © Todos los derechos reservados.</p>
+    </div>
+  </footer>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,98 @@
+<?php
+  if($_POST){
+  $nombre=$_POST["nombre"];
+  $usuario=$_POST["usuario"];
+  $email=$_POST["email"];
+  $usuario = [
+    "nombre" => $nombre,
+    "usuario" => $usuario,
+    "email" => $email,
+    "password" => password_hash($_POST["password"],PASSWORD_DEFAULT)
+  ];
+  $usuarios = json_decode(file_get_contents("json/usuarios.json"),true);
+  $usuarios[]=$usuario;
+  $usuariosFinal = json_encode($usuarios);
+  file_put_contents("json/usuarios.json",$usuariosFinal);
+  echo "Bienvenido, " . $usuario["nombre"] . "su usuario es " . $usuario["usuario"];
+}
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Acceder</title>
+    <link rel="stylesheet" href="css/estilo-header.css">
+    <link rel="stylesheet" href="css/estilo-login.css">
+    <link rel="stylesheet" href="css/estilo-footer.css">
+    <link rel="stylesheet" href="Java.js">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  </head>
+  <body style="background-image: url(../img/Background.jpg)">
+    <?php include("header.php") ?>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+      <h1 class="text-white">Acceder</h1>
+    </nav>
+    <form class="" action="login.php" method="post">
+    <div class="jumbotron jumbotron-fluid" style="background-image: url(../img/Background.jpg); margin: 0;">
+      <div class="container">
+        <div class="login-wrap">
+    	<div class="login-html">
+    		<input id="tab-1" type="radio" name="tab" class="sign-in" value="acceso.php" checked><label for="tab-1" class="tab" style="cursor: pointer";>Ingresar</label>
+    		<input id="tab-2" type="radio" name="tab" class="sign-up" value="registro.php"><label for="tab-2" class="tab" style="cursor: pointer";>Registrarme</label>
+    		<div class="login-form">
+    			<div class="sign-in-htm">
+    				<div class="group">
+    					<label for="user" class="label">Usuario</label>
+    					<input id="user" type="text" class="input">
+    				</div>
+    				<div class="group">
+    					<label for="pass" class="label">Contraseña</label>
+    					<input id="pass" type="password" class="input" data-type="password">
+    				</div>
+    				<div class="group">
+    					<input id="check" type="checkbox" class="check" checked>
+    					<label for="check"><span class="icon"></span> Recuérdame</label>
+    				</div>
+    				<div class="group">
+    					<input type="submit" class="button" value="Entrar">
+    				</div>
+    				<div class="hr"></div>
+    				<div class="foot-lnk">
+    					<a href="#recuperar" style='text-decoration:none;color:white';>Olvidaste la contraseña?</a>
+    				</div>
+    			</div>
+    			<div class="sign-up-htm">
+            <div class="group">
+              <label for="name" class="label">Nombre</label>
+              <input id="name" type="text" class="input" value="<?=$nombre?>">
+            </div>
+    				<div class="group">
+    					<label for="user" class="label">Usuario</label>
+    					<input id="user" type="text" class="input" value="<?=$usuario?>">
+    				</div>
+    				<div class="group">
+    					<label for="pass" class="label">Contraseña</label>
+    					<input id="pass" type="password" class="input" data-type="password" value="<?=$password?>">
+    				</div>
+    				<div class="group">
+    					<label for="email" class="label">Email</label>
+    					<input id="email" type="text" class="input" value="<?=$email?>">
+    				</div>
+    				<div class="group">
+    					<input type="submit" class="button" value="Registrarme">
+                                        <label class="tenesCuenta" for="tab-1" style="margin-top: 40px"!important;!important;>Ya tienes cuenta?</a>
+    				</div>
+    				
+    			</div>
+    		</div>
+    	</div>
+    </div>
+      </div>
+    </div>
+    </form>
+    <?php include("footer.php") ?>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
