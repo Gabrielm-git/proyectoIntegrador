db39c74f259fe9c4b0adbe24030b5d583677c65d
leoocaba <57098452+leoocaba@users.noreply.github.com>
2019-12-10 23:34:20 -0300

    Acá envío el header, index y botonera de home con sus css.

--- header.php
+++ header.php
@@ -1,24 +1,51 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <link rel="stylesheet" href="/css-trivia/estilo-header.css">
-  </head>
-  <body>
-    <header>
-      <nav class="navegacion">
-        <div class="titulo">
-          <h1>Trivia</h1>
-        </div>
-        <ul class="menu">
-          <li><a href="registrarvista.php">Registrate</a></li>
-          <li><a href="login.php">Iniciar Sesión</a></li>
-          <li><a href="faq.php" >Preguntas Frecuentes</a></li>
-          <li><a href="home.php">Inicio</a></li>
-        </ul>
-      </nav>
-    </header>
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <link rel="stylesheet" href="css/estilo-header.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=ZCOOL+KuaiLe&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Press+Start+2P&display=swap" rel="stylesheet">
+</head>
+<body>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: #dac300!important;">
+      <a class="navbar-brand" href="index.php">Trivia</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-  </body>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">Inicio<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="jugar.php">Jugar!</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="ranking.php">Ranking</a>
+          </li>
+          <li class="nav-item">
+           <a class="nav-link" href="FAQ.php">FAQ</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contacto.php">Contacto</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/login.php">Acceder</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Búsqueda" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">BUSCAR</button>
+        </form>
+      </div>
+    </nav>
+  </header>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
 </html>
