4978bb95564882ad3faf246de54669855eab1402
leoocaba <leoo.caballero@gmail.com>
2019-12-11 15:51:59 -0300

    Sin cambios relevantes.
    Co-Authored-By: null <leoocaba@users.noreply.github.com>

--- botonera.php
+++ botonera.php
@@ -15,8 +15,6 @@
       <div class="container">
         <div class="botones">
 
-
-
         <button type="button" onclick="location.href = 'jugar.php'" class="btn btn-primary btn-sm btn-block">JUGAR</button>
         <button type="button" onclick="location.href = 'ranking.php'" class="btn btn-primary btn-sm btn-block">RANKING</button>
         <button type="button" onclick="location.href = 'crearPregunta.php'" class="btn btn-primary btn-sm btn-block">CREÁ UNA PREGUNTA</button>
