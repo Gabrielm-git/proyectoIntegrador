1270e026c9af12b339c4bb906f0d1326fe7351b1
leoocaba <leoo.caballero@gmail.com>
2019-12-12 20:08:14 -0300

    Modificaciones en el estilo del título.
    Co-Authored-By: null <leoocaba@users.noreply.github.com>
    Co-Authored-By: null <Juanmedel7@users.noreply.github.com>

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <title>Index</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="/css/estilo-index.css">
+  <link rel="stylesheet" href="/css/estilo-home.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
   integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css?family=ZCOOL+KuaiLe&display=swap" rel="stylesheet">
@@ -14,16 +14,21 @@
 
   <div class="jumbotron jumbotron-fluid" style="background-color: yellow"!important;>
     <div class="container">
-      <h2 class="display-4">BIENVENIDO <br> A <br> TRIVIA </h2>
+      <div class="contenedor">
+        <h2 style="font-family: 'Press Start 2P'"!important;>BIENVENIDO<span>&#160;</span></h2><br>
+        <h2 style="font-family: 'Press Start 2P'"!important;>A<span>&#160;</span></h2><br>
+        <h2 style="font-family: 'Press Start 2P'"!important;>TRIVIA<span>&#160;</span></h2>
+      </div>
        <br>
       <button type="button" class="btn btn-outline-success" onclick="location.href = 'home.php'">INGRESAR</button>
-  <br>
-  <br>
-      <div class="text-center mt-5">
-        <p class="footer-alt mb-0 f-14">2019 Copyright © Todos los derechos reservados.</p>
-      </div>
+   <br>
+   <br>
+  <div class="text-center mt-5">
+      <p class="footer-alt mb-0 f-14">2019 Copyright � Todos los derechos reservados.</p>
     </div>
 
+    </div>
+ </div>
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
