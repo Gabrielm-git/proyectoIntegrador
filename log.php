56944b4a6abef51674e8302f4b64892c1db57d42
Gabrielm-git <57098544+Gabrielm-git@users.noreply.github.com>
2019-12-12 02:24:17 -0300

    .
    .

---
+++ log.php
@@ -0,0 +1,31 @@
+<?php
+
+session_start();
+require_once 'controladores/funciones.php';
+$arrayDeErrores = "";
+
+if($_POST) {
+    $arrayDeErrores = validarRegistracion($_POST);
+    if(count($arrayDeErrores) === 0) {
+        // LOGUEO AL USUARIO
+        $arrayUsuarios = abrirBBDD('usuarios.json');
+        foreach($arrayUsuarios as $usuarioJson) {
+            $userFinal = json_decode($usuarioJson, true);
+            if($_POST['email'] == $userFinal['email']) {
+                if(password_verify($_POST['password'], $userFinal['password'])) {
+                    // Crearle una sesion
+                    $_SESSION['email'] = $userFinal['email'];
+                    if(isset($_POST['recordarme']) && $_POST['recordarme'] == "on") {
+                        // Unix time
+                        setcookie('userEmail', $userFinal['email'], time() + 60 * 60 * 24 * 7);
+                        setcookie('userPass', $userFinal['password'], time() + 60 * 60 * 24 * 7);
+                    }
+                    header('Location: bienvenido.php');
+                    exit;
+                }
+            }
+        }
+    }
+}
+
+?>
---
+++ login/partials/header.php
@@ -0,0 +1,19 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Pruebas</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ml-auto">
+                <?php if(!isset($_SESSION['email'])): ?>
+                <a class="btn btn-primary mr-2" href="registro.php" role="button">Registro</a>
+                <a class="btn btn-secondary mr-2" href="login.php" role="button">Login</a>
+                <?php endif; ?>
+                <?php if(isset($_SESSION['email'])): ?>
+                <a class="btn btn-danger" href="logout.php" role="button">Log out</a>
+                <?php endif; ?>
+            </div>
+        </div>
+    </nav>
+</header>
