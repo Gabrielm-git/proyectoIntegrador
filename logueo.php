e477cc4e6e8c319859b302e108d1b6e052e452de
Gabrielm-git <57098544+Gabrielm-git@users.noreply.github.com>
2019-12-31 01:58:56 -0300

    agregado estilo

--- logueo.php
+++ logueo.php
@@ -9,20 +9,23 @@
   <link rel="stylesheet" href="css/estilo-botonera.css">
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
   </head>
-  <body>
+ <body style="font-family:Consolas; background-image: url(../img/Background.jpg);
+	font-family: Tahoma;">
 
   <?php include("header.php") ?>
-  <div class="container" >
+<div class="container border" style="background-color: #2E2E2E"!important;>
+		<br />
+
 
 <form class="form-horizontal" action="login.php" method="POST">
         <div class="form-group">
-          <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+          <label for="inputEmail3" class="col-sm-2 control-label"style="color: white"!important;>Email</label>
           <div class="col-sm-10">
             <input type="email" class="form-control" name="email" id="inputEmail3" placeholder="Email" required>
           </div>
         </div>
         <div class="form-group">
-          <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+          <label for="inputPassword3" class="col-sm-2 control-label"style="color: white"!important;>Password</label>
           <div class="col-sm-10">
             <input type="password" class="form-control" name="password" id="inputPassword3" placeholder="Password" required>
           </div>
--- registrarse.php
+++ registrarse.php
@@ -9,32 +9,34 @@
   <link rel="stylesheet" href="css/estilo-botonera.css">
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
   </head>
-  <body>
+  <body style="font-family:Consolas; background-image: url(../img/Background.jpg);
+	font-family: Tahoma;">
 
   <?php include("header.php") ?>
- <div class="container">
+<div class="container border" style="background-color: #2E2E2E"!important;>
+		<br />
 
 <form class="form-horizontal" action="registro.php" method="POST">
         <div class="form-group">
-          <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+          <label for="inputEmail3" class="col-sm-2 control-label"style="color: white"!important;	>Email</label>
           <div class="col-sm-10">
             <input type="email" class="form-control" name="email" id="inputEmail3" placeholder="email" required>
           </div>
         </div>
 <div class="form-group">
-          <label for="inputNombre3" class="col-sm-2 control-label">Nombre</label>
+          <label for="inputNombre3" class="col-sm-2 control-label"style="color: white"!important;	>Nombre</label>
           <div class="col-sm-10">
             <input type="text" class="form-control" name="nombre" id="inputNombre3" placeholder="nombre" required>
           </div>
         </div>
 <div class="form-group">
-          <label for="inputApellido3" class="col-sm-2 control-label">Apellido</label>
+          <label for="inputApellido3" class="col-sm-2 control-label"style="color: white"!important;	>Apellido</label>
           <div class="col-sm-10">
             <input type="text" class="form-control" name="apellido" id="inputApellido3" placeholder="apellido" required>
           </div>
         </div>
         <div class="form-group">
-          <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+          <label for="inputPassword3" class="col-sm-2 control-label"style="color: white"!important;	>Password</label>
           <div class="col-sm-10">
             <input type="password" class="form-control" name="password" id="inputPassword3" placeholder="password" required>
           </div>
